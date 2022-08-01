e81154ad4032dee98066b0f16d2d586adba0eb47
OthDr <otmax.vft@gmail.com>
2022-08-01 23:15:33 +0100

    minor changes

--- src/signup.js
+++ src/signup.js
@@ -10,20 +10,24 @@ async function btn_click(){
     //email_input.className += " border-red-500 border-solid border-2 ";
     console.log('clicked');
     const URL = "http://localhost:3001/v2/api/login";
-    let email = "author1@mail.com";
-    let password = "123456";
-
+    /*let email = "author1@mail.com";
+    let password = "123456";*/
+    
+    let email = email_input.value;
+    let password = password_input.value ;
+   
+    const data =  { email, password };
+    
     try {
-        /*const response = await fetch(URL ,{
-            method : 'POST',
-            headers : {
-                'Content-Type':'aplication/json',
-            },
-            body:{ email, password }
-        })
-        console.log(response);*/
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: { "Content-Type":"application/json" },
+            body:JSON.stringify(data)
+        });
+        console.log(response.status);
 
     } catch (error) {
-        
+        console.log(error);
     }
+    
 }
