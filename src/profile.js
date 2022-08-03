67a46dedf6b25e1131a46196cdf4e4d7c31aa0a4
OthDr <otmax.vft@gmail.com>
2022-08-03 13:20:51 +0100

    jwt auth

--- src/profile.js
+++ src/profile.js
@@ -12,37 +12,48 @@ let grid_bottom_articles = "";
 const token = window.localStorage.getItem("accessToken");
 
 
+
 async function getUserProfile() {
+    let id = null;
+
     try {
         const response = await fetch(`http://localhost:3001/v2/api/authors/me`, {
-            method:'GET',
-            headers:{
-                'authorization': 'Bearer'+token
+            method: 'GET',
+            headers: {
+                'authorization': 'Bearer ' + token
             }
         });
-        
-        const profile = await response.json();
-        console.log(profile);
-        first_name.innerHTML = profile.firstname;
-        last_name.innerHTML = profile.lastname;
+
+        const result = await response.json();
+        console.log(result);
+
+        if (!result.error) {
+            first_name.innerHTML = result.firstname;
+            last_name.innerHTML = result.lastname;
+            id = result.id;
+            getUserArticles(id);
+        } else {
+            window.location.href = "signup.html";
+        }
 
     } catch (error) {
         console.log(error);
+
     }
+
 }
 
-async function getUserArticles() {
+async function getUserArticles(id) {
 
     grid_container2.innerHTML = "grid_bottom_articles";
-    const id = 5;
 
     try {
         const response = await fetch(`http://localhost:3001/v2/api/authors/${id}/articles`);
         const articles = await response.json();
         console.log(articles.length);
-        let counter = 0 ;
+        let counter = 0;
         articles.forEach(element => {
-            counter ++ ;
+            counter++;
             grid_bottom_articles +=
                 `<div class="flex flex-col items-start">
                     <img class="w-full" src="${element.urlToImage}">
@@ -54,7 +65,7 @@ async function getUserArticles() {
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            <span class="font-bold">${ element.createdAt.split('T')[0] }</span> 
+                            <span class="font-bold">${element.createdAt.split('T')[0]}</span> 
                         </div>
                         <span class="border-solid border-r border-gray-400 mx-2 my-1"></span>
                         <span class="">${element.category}</span>
@@ -70,7 +81,7 @@ async function getUserArticles() {
     }
 }
 
-function SignOut(){
+function SignOut() {
     localStorage.clear();
     window.location.href = "index.html";
 }
@@ -78,4 +89,3 @@ function SignOut(){
 
 getUserProfile();
 
-getUserArticles();
